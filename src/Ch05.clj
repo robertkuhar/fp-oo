069740ee1f4544b9130d8db795dfb4b2fa2fbf57
Bob Kuhar <robertkuhar@gmail.com>
2012-11-12 23:43:30 -0800

    WIP

--- src/Ch05.clj
+++ src/Ch05.clj
@@ -1,14 +1,33 @@
-(ns Ch05
-  (:require [Ch03 :only [make] :as c3]))
+(ns Ch05)
+
+(def class-from-instance
+  (fn [instance]
+    (eval (:__class_symbol__ instance))))
+
+(def apply-message-to
+  (fn [class instance message args]
+    (apply (method-from-message message class)
+           instance args)))
+
+(def make
+  (fn [class & args]
+    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+      (apply-message-to class seeded :add-instance-values args))))
+
+(def send-to
+  (fn [instance message & args]
+    (apply-message-to (class-from-instance instance)
+                      instance message args)))
 
 (def Point
   {
+   :__own_symbol__ 'Point
    :__instance_methods__
    {
     :add-instance-values (fn [this x y]
                            (assoc this :x x :y y))
     :shift (fn [this xinc yinc]
-             (c3/make Point (+ (:x this) xinc) (+ (:y this) yinc)))
+             (make Point (+ (:x this) xinc) (+ (:y this) yinc)))
     }
    }
   )
