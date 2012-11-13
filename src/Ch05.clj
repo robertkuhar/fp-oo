538e78acc9c6db8c51c2563cdc344f3d1350d00a
Bob Kuhar <Robert_Kuhar@htc.com>
2012-11-13 14:45:43 -0800

    Use his example:  https://raw.github.com/marick/fp-oo/master/sources/class.clj

--- src/Ch05.clj
+++ src/Ch05.clj
@@ -1,23 +1,16 @@
 (ns Ch05)
 
-(def class-from-instance
-  (fn [instance]
-    (eval (:__class_symbol__ instance))))
-
-(def apply-message-to
-  (fn [class instance message args]
-    (apply (method-from-message message class)
-           instance args)))
-
 (def make
   (fn [class & args]
-    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
-      (apply-message-to class seeded :add-instance-values args))))
+    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}
+          constructor  (:add-instance-values (:__instance_methods__ class))]
+      (apply constructor seeded args))))
 
 (def send-to
   (fn [instance message & args]
-    (apply-message-to (class-from-instance instance)
-                      instance message args)))
+    (let [class (eval (:__class_symbol__ instance))
+          method (message (:__instance_methods__ class))]
+      (apply method instance args))))
 
 (def Point
   {
@@ -26,8 +19,21 @@
    {
     :add-instance-values (fn [this x y]
                            (assoc this :x x :y y))
+    :class :__class_symbol__
     :shift (fn [this xinc yinc]
              (make Point (+ (:x this) xinc) (+ (:y this) yinc)))
-    }
+    :add (fn [this other]
+           (send-to this :shift (:x other) (:y other)))
    }
-  )
+ })
+
+
+;; For exercise 4
+(def Holder
+  {
+  :__own_symbol__ 'Holder
+  :__instance_methods__
+  {
+    :add-instance-values (fn [this held] (assoc this :held held))
+  }
+})
